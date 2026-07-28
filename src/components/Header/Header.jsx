a48014583341f37d24e96c8dc46b74ee777811e3
Elad <Elad204@gmail.com>
2026-07-27 21:15:46 -0400

    update to the mobile version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import "./Header.css";
 import headerLogo from "../../images/header-logo.svg";
 import userAvatar from "../../images/avatar.svg";
+import mobileButton from "../../images/header-mobile-button.svg";
+import { useState } from "react";
 
 function Header({ handleAddButton, cityName }) {
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
   });
+  const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpened(!isMobileMenuOpened);
+  };
   return (
     <header className="header">
       <div className="header__wrap">
@@ -15,20 +22,29 @@ function Header({ handleAddButton, cityName }) {
           {currentDate}, {cityName}
         </p>
       </div>
-      <div className="header__wrap">
-        <button
-          type="button"
-          className="header__button"
-          onClick={handleAddButton}
-        >
-          + Add Cloth
+      <div className="header__user">
+        <div className="header__wrap">
+          <button
+            type="button"
+            className="header__button"
+            onClick={handleAddButton}
+          >
+            + Add Cloth
+          </button>
+          <p className="header__user-name">Terrence Tegegne</p>
+          <img
+            src={userAvatar}
+            alt="User Avatar"
+            className="header__user-avatar"
+          />
+        </div>
+        <button className="header__mobile-button">
+          <img
+            src={mobileButton}
+            alt="Mobile Menu"
+            className="header__mobile-menu"
+          />
         </button>
-        <p className="header__user-name">Terrence Tegegne</p>
-        <img
-          src={userAvatar}
-          alt="User Avatar"
-          className="header__user-avatar"
-        />
       </div>
     </header>
   );
